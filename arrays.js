f4b3e093d4dedc7aab63289c76b0872f4b05adc8
manassah Watkins <manassahwatkins@gmail.com>
2018-01-11 19:30:47 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -41,4 +41,4 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 }
 
 function removeElementFromEndOfArray(array) {
-  return array.slice(0, array.length - 1
+  return array.slice(0, array.length - 1)}
